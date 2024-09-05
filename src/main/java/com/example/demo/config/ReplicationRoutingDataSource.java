8a3f6ead662a70241203bafe740c74dd30d0abfb
CHAE <chaso1495@naver.com>
2024-09-05 18:00:35 +0900

    [feat] 라우팅 과정에 AOP 삽입 (#7)

--- src/main/java/com/example/demo/config/ReplicationRoutingDataSource.java
+++ src/main/java/com/example/demo/config/ReplicationRoutingDataSource.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import com.example.demo.config.aop.DataSourceContextHolder;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 import static org.springframework.transaction.support.TransactionSynchronizationManager.isCurrentTransactionReadOnly;
@@ -8,7 +9,13 @@ public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
-        // 현재 트랜잭션이 읽기 전용인 경우 Reader DB, 그렇지 않으면 Writer DB로 라우팅
+        // 컨텍스트에 데이터 소스 키가 설정되어 있으면 그 값을 사용
+        String dataSourceKey = DataSourceContextHolder.getDataSourceKey();
+        if (dataSourceKey != null) {
+            return dataSourceKey;
+        }
+
+        // 그렇지 않으면 트랜잭션 읽기 전용 여부에 따라 기본적으로 라우팅
         return isCurrentTransactionReadOnly() ? "reader" : "writer";
     }
 }
